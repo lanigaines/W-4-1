9848202b4e372d4a08fd095c28c24d6989ce62f1
lanigaines <lanigaines@live.com>
2024-09-29 23:07:53 -0700

    debug after comlete

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ def generate_powerrs_of_two(N):
     return result
     
 # User input
-    N + int(input("Enter and integer for power N: ")) 
+N = int(input("Enter an integer for power N: ")) 
     
-    # Generate a list of powers of two
-    result = generate_list_of_powers_of_two(N)
+# Generate a list of powers of two
+result = generate_powerrs_of_two(N)
     
 # Print the result
-print("List of powers of 2 from 0 to", N, "":")
+print("List of powers of 2 from 0 to", N, ":")
 print(result)
-    # Do not delete the return statement
-    
