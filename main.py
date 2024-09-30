49e61777feb7fe9bfda618aab1ef649567ee5199
lanigaines <lanigaines@live.com>
2024-09-29 22:39:12 -0700

    upload
    mainly

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+
+    N = int(input('Enter the number N: '))
+    result = []
+
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return result
+
+
+if __name__ == '__main__':
+    main()
